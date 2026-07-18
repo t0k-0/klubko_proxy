cdcc6b623b19835bfba4dd23206db21d1dd5f086
TKØ1 <kubelka.ak@email.cz>
2026-07-18 21:06:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-// klubko-proxy/server.js
+// server.js
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+// Allow CORS for your GitHub Pages origin
+app.use((req, res, next) => {
+  const origin = req.headers.origin || '*';
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Cookie');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use('/', createProxyMiddleware({
   target: 'https://klubko.aeroklub-kolin.cz',
   changeOrigin: true,
-  onProxyReq: (proxyReq, req, res) => {
-    // Forward cookies
+  selfHandleResponse: true, // We handle response to forward cookies
+  onProxyReq: (proxyReq, req) => {
+    // Forward cookies from browser to API
     if (req.headers.cookie) proxyReq.setHeader('cookie', req.headers.cookie);
   },
   onProxyRes: (proxyRes, req, res) => {
-    // Forward set-cookie headers back to browser
+    // Forward set-cookie from API to browser
     const cookies = proxyRes.headers['set-cookie'];
-    if (cookies) res.setHeader('set-cookie', cookies);
+    if (cookies) {
+      cookies.forEach(c => res.setHeader('Set-Cookie', c));
+    }
+    // Forward response body
+    let body = '';
+    proxyRes.on('data', chunk => body += chunk);
+    proxyRes.on('end', () => {
+      res.set(proxyRes.headers);
+      res.status(proxyRes.statusCode).send(body);
+    });
   },
 }));
 
